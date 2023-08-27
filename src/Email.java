5e00a52ef8d04b8b90ae2cf04036a772cd3be765
Camille <c.elgueta04@ufromail.cl>
2023-08-27 00:56:45 -0400

    Se agregaron más funciones para determinar si un email es válido o no

--- src/Email.java
+++ src/Email.java
@@ -1,21 +1,33 @@
 import java.util.*;
 
 public class Email {
-    public static void comprobarEmail(String email){
+    public static boolean tienePuntosExtremos(String email){  // Comprueba si hay puntos al principio o al final del email
+        for(int i = 0; i < email.length(); i ++) {
+            if (email.charAt(i) == '.' && (i == 0 || i == email.length()-1)) {
+               return true;
+            }
+        } return false;
+    }
+
+    public static boolean recorrerEmail(String email){ // Recorrer el email
         int contadorPuntos = 0;
         int contadorArroba = 0;
         for(int i = 0; i < email.length(); i ++) {
             if (email.charAt(i) == '.') {
-                contadorPuntos += 1;
+                contadorPuntos += 1; // Cuenta la cantidad de puntos
             }
             if (email.charAt(i) == '@') {
-                contadorArroba += 1;
+                contadorArroba += 1; // Cuenta la cantidad de arrobas
             }
         }
-        if(contadorPuntos > 1 && contadorArroba == 1) {
-            System.out.println("El email es válido.");
+        return contadorPuntos > 1 && contadorArroba == 1;
+    }
+
+    public static void comprobarEmail(String email) {
+        if (recorrerEmail(email) && !email.isEmpty() && email.length() < 320 && !tienePuntosExtremos(email)) {
+            System.out.println("El email es válido"); // El email es válido si cumple con las condiciones anteriores
         } else {
-            System.out.println("El email es inválido.");
+            System.out.println("El email es inválido");
         }
     }
 
@@ -24,7 +36,6 @@ public class Email {
         System.out.println("Ingrese su email: ");
         String email = teclado.nextLine();
         comprobarEmail(email);
-        System.out.println("test");
     }
 
     public static void main(String[] args) {
